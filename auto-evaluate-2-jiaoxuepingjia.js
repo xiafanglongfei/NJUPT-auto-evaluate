4d35d0f8f891a209d414ff01d071817d6535152e
夏龙飞 <1197393094@qq.com>
2019-05-24 17:22:55 +0800

    bug fix

--- auto-evaluate-2-jiaoxuepingjia.js
+++ auto-evaluate-2-jiaoxuepingjia.js
@@ -1,21 +1,27 @@
 // 教务管理系统 -> 活动报名 -> 教学评价
-var selects = $('iframeautoheight').contentDocument.getElementsByTagName("select")
-var j = 1
-var n = selects[0].childElementCount
+
+var i = 1
+var n = $('iframeautoheight').contentDocument.getElementById("pjkc").childElementCount
 console.log(n)
 var t = self.setInterval(autoEvaluate, 1000)
+
+/**
+ * @description 此函数完成一个页面的所有下拉框的选择，并单击保存按钮
+ * @author Aaronsz Xia
+ */
 function autoEvaluate() {
-    if(j == n) {
+    var selects = $('iframeautoheight').contentDocument.getElementsByTagName("select")
+    if (i == n) {
         window.clearInterval(t)
         // 取消下面这行代码的注释，那么脚本在完成所有页面的评价之后，会自动帮你点击提交按钮
         // $('iframeautoheight').contentDocument.getElementById("Button2").click()
     }
-    for (i = 1; i < selects.length; i++) {
-        selects[i].value = "好"
+    for (j = 1; j < selects.length; j++) {
+        selects[j].value = "好"
     }
-    var rand = Math.max(1, Math.floor(Math.random() * selects.length));
+    var rand = Math.max(1, Math.floor(Math.random() * selects.length))
     selects[rand].value = "较好"
     $('iframeautoheight').contentDocument.getElementById("Button1").click()
-    j++
-    console.log(j)
+    i++
+    console.log(i)
 }
